4aca17a651b2f1340c8d11224fbc648815062b45
KAVANABELAVADI <50482786+KAVANABELAVADI@users.noreply.github.com>
2020-10-29 02:37:11 +0530

    Add: Usage of Abstraction

--- SampleClassProgram.java
+++ SampleClassProgram.java
@@ -1,7 +1,37 @@
+class Car
+{
+    void print()
+    {
+        System.out.println("Car name is Maruthi");
+    }
+}
+
+abstract class Color
+{
+    abstract void paint();
+}
+class Red extends Color
+{
+    void paint()
+    {
+        System.out.println("Red color Car");
+    }
+}
+class White extends Color
+{
+    void paint()
+    {
+        System.out.println("White color Car");
+    }
+}
+
 public class SampleClassProgram
 {
     public static void main(String args[])
     {
-        System.out.println("Sample Class");
+        Car car=new Car();
+        car.print();
+        Color color=new White();
+        color.paint();
     }
 }
